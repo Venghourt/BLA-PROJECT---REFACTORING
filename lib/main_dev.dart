3a14e3b8ea7c58a201798c1c4985e2baa00e36cb
Chhoeun Venghourt <hourt2133@gmail.com>
2026-03-19 13:11:25 +0700

    BLA-201 – Repositories

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,13 +1,13 @@
-import 'package:blabla/data/repositories/locations_repository/location_repository.dart';
-import 'package:blabla/data/repositories/ride_preference_repository/ride_preference_repository.dart';
-import 'package:blabla/data/repositories/rides_repository/ride_repository.dart';
-import 'package:blabla/ui/states/ride_preference_state.dart';
 import 'package:provider/provider.dart';
-
 import 'main_common.dart';
+
+import 'data/repositories/locations_repository/location_repository.dart';
 import 'data/repositories/locations_repository/location_repository_mock.dart';
-import 'data/repositories/ride_preference_repository/ride_preference_repository_mock.dart';
+import 'data/repositories/rides_repository/ride_repository.dart';
 import 'data/repositories/rides_repository/ride_repository_mock.dart';
+import 'data/repositories/ride_preference_repository/ride_preference_repository.dart';
+import 'data/repositories/ride_preference_repository/ride_preference_repository_mock.dart';
+import 'ui/states/ride_preference_state.dart';
 
 List<InheritedProvider> get devProviders {
   return [
@@ -18,9 +18,9 @@ List<InheritedProvider> get devProviders {
     ),
     ChangeNotifierProvider<RidePreferenceState>(
       create: (context) => RidePreferenceState(
-        repository: context.read<RidePreferenceRepository>()
-      )
-    )
+        repository: context.read<RidePreferenceRepository>(),
+      ),
+    ),
   ];
 }
 
